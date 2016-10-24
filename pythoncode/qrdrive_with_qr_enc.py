b5693b7bff5788a6080c8e7e5ded37c88190c1b3
Kevin Mulligan <mulligan7t@gmail.com>
2016-10-24 22:26:34 +0200

    message

--- pythoncode/qrdrive_with_qr_enc.py
+++ pythoncode/qrdrive_with_qr_enc.py
@@ -26,24 +26,26 @@ Enc_3_B = 15              # Encoder input B: input GPIO 15
 
 
 
+rotary_counters = [0,0,0,0]
 
-Rotary_counter_0 = 0                # Start counting from 0
+rotary_counters[0] = 0                # Start counting from 0
+
 Current_0_A = 1                     # Assume that rotary switch is not 
 Current_0_B = 1                     # moving while we init software
 LockRotary_0 = threading.Lock()     # create lock for rotary switch
 
 
-Rotary_counter_1 = 0                # Start counting from 0
+rotary_counters[1] = 0                # Start counting from 0
 Current_1_A = 1                     # Assume that rotary switch is not 
 Current_1_B = 1                     # moving while we init software
 LockRotary_1 = threading.Lock()     # create lock for rotary switch
    
-Rotary_counter_2 = 0                # Start counting from 0
+rotary_counters[2] = 0                # Start counting from 0
 Current_2_A = 1                     # Assume that rotary switch is not 
 Current_2_B = 1                     # moving while we init software
 LockRotary_2 = threading.Lock()     # create lock for rotary switch
 
-Rotary_counter_3 = 0                # Start counting from 0
+rotary_counters[3] = 0                # Start counting from 0
 Current_3_A = 1                     # Assume that rotary switch is not 
 Current_3_B = 1                     # moving while we init software
 LockRotary_3 = threading.Lock()     # create lock for rotary switch
@@ -250,7 +252,7 @@ def init():
 # Rotarty encoder interrupt:
 # this one is called for both inputs from rotary switch (A and B)
 def rotary_interrupt_0(A_or_B):
-   global Rotary_counter_0, Current_0_A, Current_0_B, LockRotary_0
+   global rotary_counters[0], Current_0_A, Current_0_B, LockRotary_0
                                        # read both of the switches
    Switch_A = GPIO.input(Enc_0_A)
    Switch_B = GPIO.input(Enc_0_B)
@@ -266,16 +268,16 @@ def rotary_interrupt_0(A_or_B):
    if (Switch_A and Switch_B):                  # Both one active? Yes -> end of sequence
       LockRotary_0.acquire()                    # get lock 
       if A_or_B == Enc_0_B:                     # Turning direction depends on 
-         Rotary_counter_0 += 1                  # which input gave last interrupt
+         rotary_counters[0] += 1                  # which input gave last interrupt
       else:                                     # so depending on direction either
-         Rotary_counter_0 -= 1                  # increase or decrease counter
+         rotary_counters[0] -= 1                  # increase or decrease counter
       LockRotary_0.release()                    # and release lock
    return                                       # THAT'S IT
 
 # Rotarty encoder interrupt:
 # this one is called for both inputs from rotary switch (A and B)
 def rotary_interrupt_1(A_or_B):
-   global Rotary_counter_1, Current_1_A, Current_1_B, LockRotary_1
+   global rotary_counters[1], Current_1_A, Current_1_B, LockRotary_1
                                        # read both of the switches
    Switch_A = GPIO.input(Enc_1_A)
    Switch_B = GPIO.input(Enc_1_B)
@@ -291,16 +293,16 @@ def rotary_interrupt_1(A_or_B):
    if (Switch_A and Switch_B):                  # Both one active? Yes -> end of sequence
       LockRotary_1.acquire()                    # get lock 
       if A_or_B == Enc_1_B:                     # Turning direction depends on 
-         Rotary_counter_1 += 1                  # which input gave last interrupt
+         rotary_counters[1] += 1                  # which input gave last interrupt
       else:                                     # so depending on direction either
-         Rotary_counter_1 -= 1                  # increase or decrease counter
+         rotary_counters[1] -= 1                  # increase or decrease counter
       LockRotary_1.release()                    # and release lock
    return                                       # THAT'S IT
 
 # Rotarty encoder interrupt:
 # this one is called for both inputs from rotary switch (A and B)
 def rotary_interrupt_2(A_or_B):
-   global Rotary_counter_2, Current_2_A, Current_2_B, LockRotary_2
+   global rotary_counters[2], Current_2_A, Current_2_B, LockRotary_2
                                        # read both of the switches
    Switch_A = GPIO.input(Enc_2_A)
    Switch_B = GPIO.input(Enc_2_B)
@@ -316,16 +318,16 @@ def rotary_interrupt_2(A_or_B):
    if (Switch_A and Switch_B):                  # Both one active? Yes -> end of sequence
       LockRotary_2.acquire()                    # get lock 
       if A_or_B == Enc_2_B:                     # Turning direction depends on 
-         Rotary_counter_2 += 1                  # which input gave last interrupt
+         rotary_counters[2] += 1                  # which input gave last interrupt
       else:                                     # so depending on direction either
-         Rotary_counter_2 -= 1                  # increase or decrease counter
+         rotary_counters[2] -= 1                  # increase or decrease counter
       LockRotary_2.release()                    # and release lock
    return                                       # THAT'S IT
 
 # Rotarty encoder interrupt:
 # this one is called for both inputs from rotary switch (A and B)
 def rotary_interrupt_3(A_or_B):
-   global Rotary_counter_3, Current_3_A, Current_3_B, LockRotary_3
+   global rotary_counters[3], Current_3_A, Current_3_B, LockRotary_3
                                        # read both of the switches
    Switch_A = GPIO.input(Enc_3_A)
    Switch_B = GPIO.input(Enc_3_B)
@@ -341,9 +343,9 @@ def rotary_interrupt_3(A_or_B):
    if (Switch_A and Switch_B):                  # Both one active? Yes -> end of sequence
       LockRotary_3.acquire()                    # get lock 
       if A_or_B == Enc_3_B:                     # Turning direction depends on 
-         Rotary_counter_3 += 1                  # which input gave last interrupt
+         rotary_counters[3] += 1                  # which input gave last interrupt
       else:                                     # so depending on direction either
-         Rotary_counter_3 -= 1                  # increase or decrease counter
+         rotary_counters[3] -= 1                  # increase or decrease counter
       LockRotary_3.release()                    # and release lock
    return                                       # THAT'S IT
  
@@ -360,7 +362,7 @@ def main():
 
   print "The server is not running"
   
-  global Rotary_counter_0, LockRotary_0, Rotary_counter_1, LockRotary_1,Rotary_counter_2, LockRotary_2, Rotary_counter_3, LockRotary_3, speed_0, speed_1, speed_2, speed_3
+  global rotary_counters[0], LockRotary_0, rotary_counters[1], LockRotary_1,rotary_counters[2], LockRotary_2, rotary_counters[3], LockRotary_3, speed_0, speed_1, speed_2, speed_3
 
   TotalCount_0 = 0                            # Current TotalCount_0   
   NewCounter_0 = 0                            # for faster reading with locks           
@@ -481,32 +483,32 @@ def main():
                                   # and reset them
                                   
     LockRotary_0.acquire()               # get lock for rotary switch
-    NewCounter_0 = Rotary_counter_0      # get counter value
-    Rotary_counter_0 = 0                 # RESET IT TO 0
+    NewCounter_0 = rotary_counters[0]      # get counter value
+    rotary_counters[0] = 0                 # RESET IT TO 0
     LockRotary_0.release()               # and release lock
              
     if (NewCounter_0 !=0):               # Counter has CHANGED
        TotalCount_0 = TotalCount_0 + NewCounter_0
 
     LockRotary_1.acquire()               # get lock for rotary switch
-    NewCounter_1 = Rotary_counter_1      # get counter value
-    Rotary_counter_1 = 0                 # RESET IT TO 0
+    NewCounter_1 = rotary_counters[1]      # get counter value
+    rotary_counters[1] = 0                 # RESET IT TO 0
     LockRotary_1.release()               # and release lock
              
     if (NewCounter_1 !=0):               # Counter has CHANGED
        TotalCount_1 = TotalCount_1 + NewCounter_1
 
     LockRotary_2.acquire()               # get lock for rotary switch
-    NewCounter_2 = Rotary_counter_2      # get counter value
-    Rotary_counter_2 = 0                 # RESET IT TO 0
+    NewCounter_2 = rotary_counters[2]      # get counter value
+    rotary_counters[2] = 0                 # RESET IT TO 0
     LockRotary_2.release()               # and release lock
              
     if (NewCounter_2 !=0):               # Counter has CHANGED
        TotalCount_2 = TotalCount_2 + NewCounter_2
 
     LockRotary_3.acquire()               # get lock for rotary switch
-    NewCounter_3 = Rotary_counter_3      # get counter value
-    Rotary_counter_3 = 0                 # RESET IT TO 0
+    NewCounter_3 = rotary_counters[3]      # get counter value
+    rotary_counters[3] = 0                 # RESET IT TO 0
     LockRotary_3.release()               # and release lock
              
     if (NewCounter_3 !=0):               # Counter has CHANGED
